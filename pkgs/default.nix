351bdfc58275e28b3c47aa42ec3fb0a5c7e99c3f
lheintzmann1 <lheintzmann1@disroot.org>
2025-05-11 14:44:07 +0200

    Reorganized pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,32 +1,49 @@
-
 { pkgs }:
 
 {
   systemPackages = with pkgs; [
-    # Gnome
+    # CLI Tools
+    gh
+    git
+    git-lfs
+    tree
+
+    # Desktop Environment
+    ## Gnome
     gnome-extension-manager
     gnome-tweaks
     gnomeExtensions.gtk4-desktop-icons-ng-ding
 
-    # Misc.
-    ntfs3g
-    os-prober
-    fastfetch
-
-    # CLI
-    gh
-    git
-    git-lfs
-    fastfetch
-    tree
-
+    # Development
     vscode
-    alacritty
-
-    obsidian
 
     # Fonts
     (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-  ];
 
+    # Media
+    # (empty for future packages)
+
+    # Networking
+    # (empty for future packages)
+
+    # Productivity
+    obsidian
+
+    # Security
+    # (empty for future packages)
+
+    # System Tools
+    fastfetch
+    ntfs3g
+    os-prober
+
+    # Terminals
+    alacritty
+
+    # Utilities
+    # (empty for future packages)
+
+    # Virtualization
+    # (empty for future packages)
+  ];
 }

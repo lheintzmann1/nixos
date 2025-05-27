9ac0ee115d94177b48e7b7e8364288428d88b3d3
lheintzmann1 <lheintzmann1@disroot.org>
2025-05-27 14:15:42 +0200

    Some options has been renamed

--- home-manager/configs/vscode.nix
+++ home-manager/configs/vscode.nix
@@ -3,24 +3,26 @@
 {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      ms-python.python
-      ms-vscode.cpptools
-      bbenoist.nix
-      mshr-h.veriloghdl
-#      teros-technology.teroshdl
-      visualstudioexptteam.vscodeintellicode
-      christian-kohler.path-intellisense
-      github.copilot
-    ];
-    userSettings = {
-      "editor.fontFamily" = "'JetBrainsMono Nerd Font', monospace";
-      "editor.fontSize" = 12;
-      "editor.tabSize" = 2;
-      "editor.formatOnSave" = true;
-      "window.zoomLevel" = 1;
-      "terminal.integrated.fontFamily" = "'JetBrainsMono Nerd Font'";
-      "telemetry.telemetryLevel" = "off";
+    profiles.default = {    
+	    extensions = with pkgs.vscode-extensions; [
+      	ms-python.python
+      	ms-vscode.cpptools
+      	bbenoist.nix
+      	mshr-h.veriloghdl
+#        teros-technology.teroshdl  # waiting to be pushed in the stable channel
+        visualstudioexptteam.vscodeintellicode
+        christian-kohler.path-intellisense
+        github.copilot
+      ];
+      userSettings = {
+        "editor.fontFamily" = "'JetBrainsMono Nerd Font', monospace";
+        "editor.fontSize" = 12;
+        "editor.tabSize" = 2;
+        "editor.formatOnSave" = true;
+        "window.zoomLevel" = 1;
+        "terminal.integrated.fontFamily" = "'JetBrainsMono Nerd Font'";
+        "telemetry.telemetryLevel" = "off";
+      };
     };
   };
 }

32692472dabd145d731dec45cd2f6db5ae9eb806
lheintzmann1 <lheintzmann1@disroot.org>
2025-08-28 16:00:28 +0200

    Moved Bash config to dotfiles

--- modules/home/shell.nix
+++ modules/home/shell.nix
@@ -5,29 +5,7 @@
       enableCompletion = true;
       historyControl = [ "ignoredups" "ignorespace" ];
       historySize = 10000;
-      shellAliases = {
-        # NixOS
-        # nrs = "sudo nixos-rebuild switch --flake /etc/nixos";
-        # nrt = "sudo nixos-rebuild test --flake /etc/nixos";
-        # nrb = "sudo nixos-rebuild boot --flake /etc/nixos";
-        
-        # Git
-        gs = "git status";
-        ga = "git add";
-        gc = "git commit";
-        gp = "git push";
-        gl = "git log --oneline --graph --decorate";
-        
-        # Utils
-        ls = "eza";
-        ll = "eza -la --git";
-        la = "eza -a";
-        l = "eza -l";
-        tree = "eza --tree";
-        cat = "bat";
-        grep = "rg";
-        find = "fd";
-      };
+      bashrcExtra = builtins.readFile ../../dotfiles/shell/bashrc;
     };
   };
 }

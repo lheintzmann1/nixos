6bf4ecd7b02431ef6103b0f100e2376b4134596a
lheintzmann1 <lheintzmann1@disroot.org>
2025-08-29 16:13:46 +0200

    Gnome: Enable Dash to Dock and Blur my Shel with defaults; hide mounts

--- modules/core/desktop.nix
+++ modules/core/desktop.nix
@@ -23,5 +23,21 @@
   services.xserver.desktopManager.gnome.extraGSettingsOverrides = ''
     [org.gnome.desktop.wm.preferences]
     button-layout=':minimize,maximize,close'
+
+    [org.gnome.shell]
+    enabled-extensions=['dash-to-dock@micxgx.gmail.com', 'blur-my-shell@aunetx', 'user-theme@gnome-shell-extensions.gcampax.github.com']
+
+    [org.gnome.shell.extensions.dash-to-dock]
+    dock-position='BOTTOM'
+    dash-max-icon-size=36
+    click-action='minimize-or-previews'
+    show-trash=true
+    show-mounts=false
+
+    [org.gnome.shell.extensions.blur-my-shell]
+    sigma=30
+    brightness=0.8
+    style-components=['panel', 'dash', 'overview', 'applications']
+    hacks-level=2
   '';
 }

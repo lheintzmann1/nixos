00024f63898f09890a7e54a23772d610adc2e41b
lheintzmann1 <lheintzmann1@disroot.org>
2025-09-14 22:02:34 +0200

    Git: Let gh manage its own config after initial auth

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -14,5 +14,7 @@
   xdg.configFile."git/ignore".source = ../../dotfiles/git/gitignore;
   home.file.".gitconfig".source = ../../dotfiles/git/gitconfig; # ensure Git sees it if XDG isn't used
 
-  xdg.configFile."gh/config.yml".source = lib.mkForce ../../dotfiles/gh/config.yml;
+  xdg.configFile."gh/config.yml".source = lib.mkIf (!builtins.pathExists "${config.home.homeDirectory}/.config/gh/config.yml") {
+	source =  ../../dotfiles/gh/config.yml;
+  }
 }

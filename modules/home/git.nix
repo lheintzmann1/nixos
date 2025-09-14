b062c34b884bc5d0d70529b9a0b793ca6fbaecd6
lheintzmann1 <lheintzmann1@disroot.org>
2025-09-14 22:14:07 +0200

    gh: Not using dotfiles config anymore to avoid auth issues

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -13,8 +13,4 @@
   xdg.configFile."git/config".source = ../../dotfiles/git/gitconfig;
   xdg.configFile."git/ignore".source = ../../dotfiles/git/gitignore;
   home.file.".gitconfig".source = ../../dotfiles/git/gitconfig; # ensure Git sees it if XDG isn't used
-
-  xdg.configFile."gh/config.yml".source = lib.mkIf (!builtins.pathExists "${config.home.homeDirectory}/.config/gh/config.yml") {
-	source =  ../../dotfiles/gh/config.yml;
-  };
 }
